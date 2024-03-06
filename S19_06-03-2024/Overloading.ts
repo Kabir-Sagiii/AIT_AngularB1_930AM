dd49c08816d5a29238c0d8bfe0d2d3caed540e37
Kabir <sagithedeveloper@gmail.com>
2024-03-06 10:44:38 +0530

    Added the code

---
+++ S19_06-03-2024/Overloading.ts
@@ -0,0 +1,15 @@
+class Users {
+
+    name:string;
+    id:number;
+
+    // display():void {
+    //     console.log(this.name,this.id)
+    // }
+
+    // display(name:string,id:number):void {
+    // this.name = name 
+    // this.id = id
+    // console.log(this.name,this.id)
+    // }
+}
